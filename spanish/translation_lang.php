adbd15f9446b4e82d6b732d29d7c1321a85daf42
Natan Felles <natanfelles@gmail.com>
2016-03-25 22:59:36 -0300

    Add spanish

---
+++ spanish/translation_lang.php
@@ -0,0 +1,242 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+// header
+$lang['features'] = 'Destacados';
+$lang['prices'] = 'Precios';
+$lang['addons'] = 'Diferenciales';
+$lang['login'] = 'Ingrese';
+
+// company
+$lang['company'] = 'Negocio';
+$lang['about_us'] = 'Sobre Nosotros';
+$lang['blog'] = 'Blog';
+$lang['logos'] = 'Logos';
+$lang['careers'] = 'Empleos';
+$lang['contact'] = 'Contacto';
+$lang['contact_us'] = 'Contáctenos';
+$lang['documentation'] = 'Documentación';
+$lang['all_rights_reserved'] = 'Todos los derechos reservados.';
+
+// home_index
+$lang['welcome_title'] = 'Bienvenidos a la Revolución!';
+$lang['welcome_description'] = 'Las mejores herramientas de Comercio Electrónico se reunieron en un solo lugar.';
+$lang['join_us'] = 'Únete';
+$lang['terms'] = 'Términos de Uso';
+$lang['terms_click'] = 'Al hacer clic en "Crear Cuenta" está de acuerdo con el';
+
+// quick_prices
+$lang['promo_price'] = 'Super Store Starter por Sólo U$10/mes';
+$lang['see_plans'] = 'Ver Planes y Precios';
+
+// quick_register
+$lang['join_today'] = 'Únete al Super eCommerce hoy';
+$lang['create_an_account'] = 'Crear una Cuenta';
+
+// register form
+$lang['firstname'] = 'Nombre';
+$lang['lastname'] = 'Apellido';
+$lang['username'] = 'Nombre de Usuario';
+$lang['email'] = 'E-mail';
+$lang['password'] = 'Contraseña';
+$lang['password_confirm'] = 'Confirme la Contraseña';
+$lang['create_account'] = 'Crear Cuenta';
+$lang['username_error_unique'] = 'Ya existe este Nombre de Usuario.';
+$lang['username_error_regex'] = 'El campo Nombre de Usuario no puede contener espacios.';
+$lang['email_error_isemail'] = 'El campo E-mail debe contener una dirección de email válida.';
+$lang['customer_no_error_unique'] = '¡Huy! Muchos de los registros al mismo tiempo. Inténtalo de nuevo.';
+$lang['password_error'] = 'Las Contraseñas deben ser iguales, tener al menos 6 caracteres y Green Force.';
+$lang['connection_error'] = 'Servicio no disponible. Por favor, inténtelo de nuevo más tarde.';
+$lang['register_errors'] = 'Por favor, corrija los siguientes errores:';
+$lang['username_password_error'] = 'Nombre de Usuario o Contraseña incorrecta.';
+$lang['create_account_success'] = '¡Enhorabuena! Su cuenta ha sido creada con éxito!';
+$lang['create_account_success_msg'] = 'Estudio sobre el uso de Super eCommerce o acceder a su cuenta:';
+$lang['hello'] = '¡Hola';
+$lang['create_account_free'] = 'Crear una cuenta. Es gratis.';
+$lang['password_strength'] = 'Contraseña Fuerza';
+$lang['password_confirm_check'] = 'Confirmación de la Contraseña';
+$lang['password_confirm_check_ok'] = 'Las Contraseñas son iguales.';
+$lang['password_confirm_check_error'] = 'Las Contraseñas son diferentes.';
+
+// plans
+$lang['month'] = 'mes';
+$lang['ram'] = 'RAM';
+$lang['storage'] = 'Almacenamiento';
+$lang['bandwidth'] = 'Tráfico Banda';
+$lang['email_accounts'] = 'Cuentas de E-mail';
+$lang['cores'] = 'Procesadores';
+$lang['databases'] = 'Bases de Datos';
+$lang['ftp_users'] = 'Usuarios de FTP';
+$lang['admins'] = 'Administradores';
+$lang['login'] = 'Ingrese';
+$lang['back'] = 'Volver';
+$lang['sign'] = 'Contratar';
+
+// products
+$lang['products'] = 'Productos';
+$lang['superstore'] = 'Super Store';
+$lang['webhosting'] = 'Web Hosting';
+$lang['vpsmanaged'] = 'VPS Managed';
+$lang['developersuite'] = 'Developer Suite';
+
+// resources
+$lang['resources'] = 'Medios';
+$lang['controlpanel'] = 'Control Panel';
+$lang['webmail'] = 'Webmail';
+$lang['filemanager'] = 'File Manager';
+$lang['dbmanager'] = 'DB Manager';
+$lang['superadmin'] = 'Superadmin';
+$lang['affiliation'] = 'Asociación';
+$lang['controlpanel_desc'] = 'Maneje todas sus aplicaciones a través del panel de control. Situado en una manera práctica e intuitiva sus cuentas de correo electrónico, bases de datos, servidores FTP, sitios web, y DNS. Además de realizar la facturación como un cliente o socio.';
+$lang['webmail_desc'] = 'Ya es hora de profesionalizar su marca en Internet con una dirección de correo personalizado. Comunicarse con estilo en un ambiente desarrollado con el fin de afirmar su negocio y le ayudará a mejorar aún más su éxito.';
+$lang['filemanager_desc'] = 'El gestor de archivos permite la edición en línea por tres editores diferentes; Código Texto y Visual. Así que se puede editar y crear sus aplicaciones en cualquier lugar sólo por tener acceso a Internet y un navegador web.';
+$lang['dbmanager_desc'] = 'Manejo de bases de datos por Shell es para los maestros. Pero una vez que los días pasan volando, el Data Base Manager es su mejor amigo para dominar MySQL. Importación, exportación, ejecutar comandos SQL de acuerdo a su necesidad.';
+$lang['superadmin_desc'] = 'Para ustedes que quieren ofrecer sus productos en línea con confianza. El Superadmin permite la administración de tu comercio electrónico con la funcionalidad y seguridad.';
+$lang['affiliation_desc'] = '¡Conviértete en nuestro socio y ayudar a abrir miles de tiendas en línea de todo el mundo. Las ganancias son mensuales y cubren 30% del producto.';
+
+// features
+$lang['featues_title'] = 'Simple. Complete. Potente.';
+$lang['featues_desc_1'] = 'Piense en un sistema listo para su tienda y mostrarlo al mundo.';
+$lang['featues_desc_2'] = 'Piense en un sistema listo para recibir a sus productos y aumentar sus ventas.';
+$lang['featues_desc_3'] = 'La suite más completa de herramientas de marketing online a su alcance, en cualquier lugar.';
+$lang['to_developers'] = 'De Desarrolladores';
+$lang['to_sellers'] = 'De Emprendedores';
+
+// docs
+$lang['know_more'] = 'Conocer más...';
+$lang['knowledge_base'] = 'Base de Conocimiento';
+$lang['results_to'] = 'Resultados para:';
+$lang['results_not_found'] = 'No se encontraron resultados. Hacer una nueva búsqueda o haga clic';
+$lang['here'] = 'aquí';
+$lang['results_back'] = 'para volver.';
+$lang['search_here'] = 'Haga su búsqueda aquí...';
+$lang['documentation'] = 'Documentación';
+$lang['starting'] = 'Comenzando';
+$lang['billing'] = 'Facturas';
+$lang['category_no_docs'] = 'No hay documentos aquí.';
+
+// order
+$lang['authentication'] = 'Autenticación';
+$lang['authentication_desc'] = 'Identifíquese para continuar o';
+$lang['domain'] = 'Dominio';
+$lang['order_confirm'] = 'Realizar Pedido';
+$lang['order_success'] = 'Solicitud completado!';
+$lang['caution'] = '¡Atención';
+$lang['order_caution'] = 'Para activar los servicios de esta firma, el pago completo a través del Control Panel o el botón de abajo.';
+$lang['order_pay'] = 'Efectuar Pago';
+
+// error 404
+$lang['error_404_message'] = '¡Huy! Ver dónde se termina. Esta página no existe.';
+
+// company
+$lang['validation_question'] = '¿Cuánto es';
+$lang['validation_question_error'] = 'El campo de Validación Sum es incorrecto.';
+$lang['success_career'] = 'Si se selecciona su currículum se comunicará con usted. Gracias.';
+$lang['success_contact'] = 'Nos pondremos en contacto con usted lo antes posible. Gracias.';
+$lang['qualifications'] = 'Calificaciones';
+$lang['subject'] = 'Asunto';
+$lang['message'] = 'Mensaje';
+$lang['locale'] = 'Ubicación';
+$lang['qualifications'] = 'Calificaciones';
+$lang['your_career'] = 'Vacante Destinado';
+$lang['need_careers'] = 'Aplicar';
+$lang['career_me'] = 'Espacios Abiertos';
+$lang['description'] = 'Descripción';
+$lang['requirements'] = 'Requerimientos';
+$lang['web_designer'] = 'Web Designer';
+$lang['web_designer_desc'] = 'Trabajos profesionales en el front-end de desarrollo de aplicaciones web a distancia y acceso remoto.';
+$lang['web_designer_req'] = 'Dominio en HTML, CSS, JavaScript y edición de imágenes con Photoshop o Fireworks. ';
+$lang['reseller'] = 'Representante de Ventas';
+$lang['reseller_desc'] = 'Trabajos profesionales en representación de la compañía en su localidad.';
+$lang['reseller_req'] = 'Cualificaciones específicas para la posición.';
+$lang['portfolio'] = 'Currículum';
+$lang['send'] = 'Enviar';
+$lang['system_status'] = 'Estatus del Sistema';
+$lang['system_status_desc'] = 'Comprobar el Estatus del Sistema en el servidor.';
+$lang['hostname'] = 'Hostname';
+$lang['hostname_error'] = 'El campo hostname debe contener un nombre de host válido.';
+$lang['server'] = 'Servidor';
+$lang['status'] = 'Estatus';
+$lang['verify'] = 'Verificar';
+$lang['commerce'] = 'Comercial';
+$lang['support'] = 'Soporte';
+$lang['visite_us'] = 'Visítenos';
+$lang['logos_desc'] = 'Descargar logos del Super eCommerce para utilizarlos en sus campañas de asociación.';
+$lang['contact_message'] = 'Si ya tienes una cuenta, póngase en contacto con nosotros a través del Control Panel. De lo contrario, póngase en contacto con nosotros mediante el siguiente formulario.';
+$lang['careers_message'] = 'No somos tan amables como nos mantenemos enfocados en resultados. Usted no puede pedir que ir al bar con nosotros. Puede que tenga que cambiar sus vidas para formar parte del equipo. Tal vez usted no se preocupa por eso.';
+$lang['careers_message_2'] = 'Si es así, asegúrese de solicitar una vacante. Si tiene potencial, por supuesto.';
+$lang['success'] = 'Éxito!';
+$lang['about_line_1'] = 'Nos encanta trabajar con la tecnología y ayudar a nuestros clientes a alcanzar sus metas en un mercado tan competitivo como el actual.';
+$lang['about_line_2'] = 'Estamos fascinados por las computadoras y la programación. Vivimos para resolver problemas, desarrollar aplicaciones que son útiles en el día a día y nos encanta hacer que las cosas sucedan. Y queremos compartir con la gente.';
+$lang['about_line_3'] = 'Esta ha sido nuestra filosofía desde que la compañía fue registrada en febrero 2015 por el CEO-fundador y programador Nataniel S. Felles.';
+$lang['about_line_4'] = 'El Super eCommerce tiene este ideal y lo sigue siendo para siempre.';
+$lang['status_on'] = 'Todos los servicios están online!';
+$lang['status_off'] = 'Hay servicios offline!';
+$lang['status_unknow'] = 'No se encontró servidor.';
+$lang['status_verify'] = 'Introduzca los datos necesarios y haga clic en Verificar.';
+
+// Prices
+$lang['special_to_agencies'] = 'Especialmente para Diseñadores Web y Agencias';
+$lang['details'] = 'Detalles';
+$lang['plans'] = 'Planes';
+// Prices / Super Store
+$lang['best_to_entrepreneurs'] = 'La Mejor Opción para los Empresarios';
+$lang['world_platform'] = 'Reconocido plataforma mundial';
+$lang['some_info'] = 'Algunas Informaciones';
+$lang['ss_desc_1'] = 'Decidimos llamar a nuestro sistema de compra Super Store para el comercio electrónico debido a su capacidad distintiva y robusto mantenido con la base de la plataforma Magento Community.';
+$lang['ss_desc_2'] = 'Si usted ya tiene una tienda de Magento, puede importar sus productos en el sistema en nuestros servidores. O si lo desea, hacemos la migración.';
+$lang['ss_desc_3'] = 'Va a tener un servidor dedicado con entrega 100% de los recursos contratados y soporte en línea las 24 horas del día.';
+$lang['ss_desc_4'] = 'Para adquirir diferentes temas, indique la';
+$lang['ss_li_1'] = 'La plataforma de comercio electrónico más completa de hoy';
+$lang['ss_li_2'] = 'Registro ilimitado de productos';
+$lang['ss_li_3'] = 'Posibilidad de varias tiendas en un solo sitio web';
+$lang['ss_li_4'] = 'Sistema de catálogo detallado';
+$lang['ss_li_5'] = '6 tipos de productos';
+$lang['ss_li_6'] = 'Clientes y promociones gestión';
+$lang['ss_li_7'] = 'Sistema de boletins informativos integrado';
+$lang['ss_li_8'] = 'Edición de páginas de contenido';
+$lang['ss_li_9'] = 'Los informes del sistema y sondeos';
+$lang['ss_li_10'] = 'Camino secreto en la administración';
+$lang['ss_li_11'] = 'Acceso FTP y Base de Datos';
+$lang['ss_li_12'] = 'Diversas formas de pago y envío';
+$lang['ss_li_13'] = 'Gestión organizado por nivel de acceso';
+$lang['ss_li_14'] = 'Los administradores con funciones específicas';
+$lang['ss_li_15'] = 'Las copias de seguridad diarias';
+$lang['ss_li_16'] = 'Optimizado para rendimiento extremo';
+$lang['ss_li_17'] = 'Pre-configurado para la fácil adaptación';
+// Prices / Web Hosting
+$lang['server_performance'] = 'Servidores optimizados para rendimiento extremo';
+$lang['wh_desc_1'] = 'El Super eCommerce es el escenario perfecto para la celebración de las plataformas que requieren una gran cantidad de recursos del sistema, como Magento y Wordpress. Pero cualquier aplicación escrita en PHP se puede ejecutar.';
+$lang['wh_li_1'] = 'Varias versiones de PHP disponibles';
+$lang['wh_li_2'] = 'Límite de Memoria de 256 MB';
+$lang['wh_li_3'] = 'Optimización de MySQL Alto Rendimiento';
+$lang['wh_li_4'] = 'Servidor Web compilado con Google ngx_pagespeed';
+$lang['wh_li_5'] = 'Gestión Web de E-mail, FTP y Bases de Datos';
+$lang['wh_li_6'] = 'Posibilidad de Arriba / Downgrade';
+$lang['wh_li_7'] = 'Copias de Seguridad Programadas';
+$lang['wh_li_8'] = 'Autofirmado SSL Gratuito';
+$lang['wh_li_9'] = '100 Límite de Subdominios';
+$lang['wh_li_10'] = 'Protección por contraseña para ciertos directorios';
+$lang['wh_li_11'] = 'Estadísticas de las visitas';
+$lang['wh_li_12'] = 'Subdominio para Pruebas';
+$lang['wh_li_13'] = 'Ruta de ejecución de PHP fuera del acceso público';
+$lang['wh_li_14'] = 'Cada cuenta de email con 500 MB de almacenamiento';
+
+// Prices / FAQ
+$lang['faq_title'] = 'Como el trabajo de la firma?';
+$lang['faq_q1'] = 'Puedo evaluar antes de firmar?';
+$lang['faq_q2'] = 'Puedo resolver una evaluación?';
+$lang['faq_q3'] = '¿Qué recursos podemos utilizar?';
+$lang['faq_q4'] = '¿Existe un contrato?';
+$lang['faq_q5'] = 'Usted tiene cargos extra?';
+$lang['faq_q6'] = '¿Puedo cambiar mi plan?';
+$lang['faq_a1'] = '¡Sí! Cada nuevo usuario tiene derecho a una prueba gratuita durante 7 días en un Super Store y Web Hosting. No requerimos algún tipo de compromiso. Simplemente haga clic en el botón "Evaluar" y solicitud de revisión.';
+$lang['faq_a2'] = 'Sí. Siempre y cuando usted elige un plan regular dentro del período de evaluación. Cuando el período de evaluación final, comenzó a facturar las cuotas mensuales para el plan seleccionado.';
+$lang['faq_a3'] = 'Todos los recursos disponibles de acuerdo a su producto. El Superadmin pertenece exclusivamente a Super Store, las otras características se incluyen en todos los planes.';
+$lang['faq_a4'] = 'Planes de Super eCommerce se ofrecen sobre una base mes a mes. No hay contratos físicos para firmar y usted puede cancelar un plan en cualquier momento.';
+$lang['faq_a5'] = 'No. Nosotros no cobramos ningún cargo adicional. La única tasa que paga es el cargo mensual de su plan.';
+$lang['faq_a6'] = 'Sí. Usted puede solicitar el cambio de planes en cualquier momento. Siempre que el plan solicitado pertenece al mismo producto.';
+$lang['avaliate'] = 'Evaluar';
+$lang['faq_help'] = 'Si en caso de que haya cualquier pregunta adicional. ';
+$lang['or'] = 'o';
+$lang['define_domain'] = 'Define un dominio';
+$lang['define_domain_msg'] = 'Dejar en blanco para usar el subdominio temporal';
